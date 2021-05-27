06a8470e84696326a881d96c7309248508deb5c8
gaurangrshah <gaurang.r.shah@gmail.com>
2021-05-27 19:35:40 -0400

    adds airtable fetching logic

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,96 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ page }) {
+  console.log("🚀 ~ file: index.js ~ line 5 ~ Home ~ page", page);
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>CMS Splash Page</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href='https://nextjs.org'>Next.js!</a>
         </h1>
+        <div className={styles.card}>
+          <h3>Airtable content &rarr;</h3>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <pre>
+            <code>{JSON.stringify(page, null, 2)}</code>
+          </pre>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  // asynchronously import neccessary modules so we can avoid any client side compilation
+  const { getPage, getRecordById, getRelatedRecords } = await import(
+    "../lib/airtable"
+  );
+
+  // query inital data from airtable
+  const page = await getPage("launch");
+  const [launch] = page;
+
+  // get related seo metadata
+  if (launch?.fields?.seoId?.length) {
+    const seo = await getRelatedRecords("seo", launch?.fields?.seoId);
+
+    if (seo[0]?.fields?.mediaId.length) {
+      // get related media for seo og:image
+      const ogImage = await getRecordById("media", seo[0]?.fields?.mediaId[0]);
+      seo[0].fields.ogImage = ogImage;
+    }
+    launch.seo = seo;
+  }
+
+  // get related data if there is any
+  if (launch?.fields?.sectionId?.length) {
+    const sections = await getRelatedRecords(
+      "sections",
+      launch?.fields?.sectionId
+    );
+
+    // get all blocks for each section and their related media
+    if (sections?.length) {
+      launch.sections = await Promise.all(
+        await sections.map(async (section) => {
+          if (section?.fields?.blockId?.length) {
+            const blocks = await getRelatedRecords(
+              "blocks",
+              section?.fields?.blockId
+            );
+            if (blocks?.length) {
+              section.blocks = await Promise.all(
+                await blocks.map(async (block) => {
+                  if (block?.fields?.mediaId?.length) {
+                    const media = await getRelatedRecords(
+                      "media",
+                      block?.fields?.mediaId
+                    );
+
+                    if (media?.length) {
+                      block.media = media;
+                    }
+                  }
+                  return block;
+                })
+              );
+            }
+          }
+          return section;
+        })
+      );
+    }
+
+    return {
+      props: {
+        page: launch,
+      },
+    };
+  }
 }
