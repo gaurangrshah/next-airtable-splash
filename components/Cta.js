b07154ba91f6205da7fb2ad9753380681e0c653e
gshah2020 <gaurang.r.shah@gmail.com>
2021-05-28 02:42:03 -0400

    Create Cta.js

---
+++ components/Cta.js
@@ -0,0 +1,35 @@
+import styles from "../styles/Cta.module.css";
+
+export const Cta = ({ data }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const input = e.target.querySelector("input");
+    console.log(input.value);
+  };
+
+  return (
+    <div className={styles.cta}>
+      {data?.map((block) => (
+        <div key={block?.id}>
+          <h2>{block?.fields?.title}</h2>
+          <p>{block?.fields?.excerpt}</p>
+          <form onSubmit={handleSubmit}>
+            <div className={styles.inputGroup}>
+              <input
+                type='email'
+                placeholder='you@youremail.com'
+                required
+                className={styles.input}
+              />
+              <div className={styles.inputRight}>
+                <button type='submit' className={styles.submit}>
+                  Sign up now
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      ))}
+    </div>
+  );
+};
