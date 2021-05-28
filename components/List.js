5489e5c1b801f5cde388b38ba8669859897f040b
gshah2020 <gaurang.r.shah@gmail.com>
2021-05-28 02:42:45 -0400

    Create List.js

---
+++ components/List.js
@@ -0,0 +1,28 @@
+import styles from "../styles/List.module.css";
+
+export const List = ({ data }) => {
+  return (
+    <ul className={styles.list}>
+      {data?.map((block) => (
+        <ListItem
+          key={block?.id}
+          className={styles.listItem}
+          img={block?.media[0]?.fields}
+        >
+          {block?.fields?.title}
+        </ListItem>
+      ))}
+    </ul>
+  );
+};
+
+export const ListItem = ({ img, children }) => {
+  return (
+    <li className={styles.listItem}>
+      <span className={styles.icon}>
+        <img src={img?.url[0].url} alt={img?.alt} width='28px' height='28px' />
+      </span>
+      {children}
+    </li>
+  );
+};
