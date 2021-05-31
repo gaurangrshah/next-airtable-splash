9bd1bc65ed3fe17c22e98a95475d7f665c33ccc2
gshah2020 <gaurang.r.shah@gmail.com>
2021-05-30 20:41:44 -0400

    Update Cta.js

--- components/Cta.js
+++ components/Cta.js
@@ -1,6 +1,6 @@
 import styles from "../styles/Cta.module.css";
 
-export const Cta = ({ data }) => {
+export const Cta = ({ data, filter = [] }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const input = e.target.querySelector("input");
@@ -13,21 +13,23 @@ export const Cta = ({ data }) => {
         <div key={block?.id}>
           <h2>{block?.fields?.title}</h2>
           <p>{block?.fields?.excerpt}</p>
-          <form onSubmit={handleSubmit}>
-            <div className={styles.inputGroup}>
-              <input
-                type='email'
-                placeholder='you@youremail.com'
-                required
-                className={styles.input}
-              />
-              <div className={styles.inputRight}>
-                <button type='submit' className={styles.submit}>
-                  Sign up now
-                </button>
+          {filter.includes("withForm") && (
+            <form onSubmit={handleSubmit}>
+              <div className={styles.inputGroup}>
+                <input
+                  type='email'
+                  placeholder='you@youremail.com'
+                  required
+                  className={styles.input}
+                />
+                <div className={styles.inputRight}>
+                  <button type='submit' className={styles.submit}>
+                    Sign up now
+                  </button>
+                </div>
               </div>
-            </div>
-          </form>
+            </form>
+          )}
         </div>
       ))}
     </div>
