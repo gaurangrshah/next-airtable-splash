7cd10cce0a09e72b1c4e9b34c668fd6e3b19123d
gshah2020 <gaurang.r.shah@gmail.com>
2021-05-30 20:46:59 -0400

    Update List.js

--- components/List.js
+++ components/List.js
@@ -1,6 +1,7 @@
+import Image from "next/image";
 import styles from "../styles/List.module.css";
 
-export const List = ({ data }) => {
+export const List = ({ data, ...rest }) => {
   return (
     <ul className={styles.list}>
       {data?.map((block) => (
@@ -8,6 +9,7 @@ export const List = ({ data }) => {
           key={block?.id}
           className={styles.listItem}
           img={block?.media[0]?.fields}
+          {...rest}
         >
           {block?.fields?.title}
         </ListItem>
@@ -16,12 +18,20 @@ export const List = ({ data }) => {
   );
 };
 
-export const ListItem = ({ img, children }) => {
+export const ListItem = ({ img, filter = [], children }) => {
   return (
     <li className={styles.listItem}>
-      <span className={styles.icon}>
-        <img src={img?.url[0].url} alt={img?.alt} width='28px' height='28px' />
-      </span>
+      {filter?.includes("withIcons") && (
+        <span className={styles.icon}>
+          <Image
+            layout='intrinsic'
+            src={img?.url[0].url}
+            alt={img?.alt}
+            width='28px'
+            height='28px'
+          />
+        </span>
+      )}
       {children}
     </li>
   );
