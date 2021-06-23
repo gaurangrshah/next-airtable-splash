a6e625e2843b7ff763670dcf1ede04b93ba10a70
gaurangrshah <gaurang.r.shah@gmail.com>
2021-06-23 14:38:12 -0400

    completes post2 starter

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,19 @@
 import Head from "next/head";
-
-import { Splash } from "../components/Splash";
-import { removeNullValues } from "../utils/data-helpers";
-
 import styles from "../styles/Home.module.css";
 
-export default function Home({ rows }) {
+export default function Home() {
   return (
-    <div className={styles.pageWrapper}>
+    <div className={styles.container}>
       <Head>
-        <title>{rows[0]?.seo_title}</title>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-        <meta name='description' content={rows[0]?.seo_description} />
-        <meta property='og:title' content={rows[0]?.seo_title} />
-        <meta property='og:site_name' content={rows[0]?.seo_sitename} />
-        <meta property='og:url' content={rows[0]?.seo_url} />
-        <meta property='og:description' content={rows[0]?.seo_description} />
-        <meta property='og:image' content={rows[0]?.url} />
+        <title>Create Next App</title>
         <link rel='icon' href='/favicon.ico' />
-        <link rel='cannonical' href={rows[0]?.url} />
       </Head>
 
       <main className={styles.main}>
-        <Splash data={rows} />
+        <h1 className={styles.title}>
+          Welcome to <a href='https://nextjs.org'>Next.js!</a>
+        </h1>
       </main>
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const { getSections } = await import("../lib/pg");
-
-  const response = await getSections();
-  const rows = removeNullValues(response?.rows);
-
-  return {
-    props: {
-      rows,
-    },
-  };
 }
