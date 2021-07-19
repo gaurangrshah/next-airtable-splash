f6ddb91f270e9a1323636cced38daed301ef0ceb
gaurangrshah <gaurang.r.shah@gmail.com>
2021-07-18 23:08:17 -0400

    strips landing page

--- pages/landing.js
+++ pages/landing.js
@@ -1,120 +1,19 @@
-import { SEO } from "../components/SEO";
-import { Container, Section, Row } from "../components/Containers";
-import {
-  BrandList,
-  BenefitsCards,
-  FeaturedBenefit,
-  Header,
-  Hero,
-  Pricing,
-  RiskCta,
-  Testimonial,
-  FooterCta,
-} from "../components/landing";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
-import { PageBuild, sortRows } from "../utils/data-helpers";
-import styles from "../styles/Landing.module.css";
-
-export default function Landing({ page = {} }) {
-  const { seo, page: rows } = page;
-
-  const [
-    { landingHero },
-    { landingBrandList },
-    { landingBenefitsIntro },
-    { landingBenefits },
-    { landingFeaturedTestimonial },
-    { landingFeaturedBenefit1 },
-    { landingFeaturedBenefit2 },
-    { landingFeaturedBenefit3 },
-    { landingCtaRisk },
-    { landingPricing },
-    { landingCtaUrgency },
-  ] = sortRows(rows);
-
-  const [featuredTestimonial] = landingFeaturedTestimonial;
-
+export default function Home() {
   return (
-    <div className={styles.pageWrapper}>
-      <SEO seo={seo} />
-      <Header brandName={seo?.title} />
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
       <main className={styles.main}>
-        <Section style={{ padding: "2em", textAlign: "center" }}>
-          <Hero block={landingHero[0]?.block} />
-          <Container>{<BrandList data={landingBrandList} />}</Container>
-        </Section>
-        <Section className='secondary-light'>
-          <BenefitsCards
-            headingBlock={landingBenefitsIntro[0].block}
-            // sort by block order ASC
-            data={landingBenefits.sort((a, b) =>
-              a.block.order > b.block.order ? 1 : -1
-            )}
-          />
-          <Testimonial block={featuredTestimonial.block} />
-        </Section>
-        <Section>
-          <Container>
-            <FeaturedBenefit
-              block={landingFeaturedBenefit1[0].block}
-              alternate={landingFeaturedBenefit1[0]["filter"].includes(
-                "reverse"
-              )}
-            />
-          </Container>
-          <div style={{ width: "100%", background: "var(--primary-light)" }}>
-            <Container>
-              <FeaturedBenefit
-                block={landingFeaturedBenefit2[0].block}
-                alternate={landingFeaturedBenefit2[0]["filter"].includes(
-                  "reverse"
-                )}
-              />
-            </Container>
-          </div>
-          <Container>
-            <FeaturedBenefit
-              block={landingFeaturedBenefit3[0].block}
-              alternate={landingFeaturedBenefit3[0]["filter"].includes(
-                "reverse"
-              )}
-            />
-          </Container>
-          <div style={{ width: "100%", background: "var(--secondary-light)" }}>
-            {/* <Container> */}
-            <RiskCta block={landingCtaRisk[0].block} />
-            {/* </Container> */}
-          </div>
-        </Section>
-        <Section>
-          <Container>
-            <Pricing data={landingPricing} />
-          </Container>
-        </Section>
-        <Section className='primary-lighter'>
-          <Container style={{ margin: "4em auto" }}>
-            <FooterCta block={landingCtaUrgency[0].block} />
-          </Container>
-        </Section>
+        <h1 className={styles.title}>
+          Welcome to <a href='https://nextjs.org'>Next.js!</a>
+        </h1>
       </main>
-      <footer className={styles.footer}>
-        {" "}
-        &copy;
-        <span>Uptime Sentry</span> {new Date().getFullYear()}
-      </footer>
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const { getPage } = await import("../lib/pg");
-
-  const response = await getPage("landing");
-  const page = PageBuild(response?.rows);
-
-  return {
-    props: {
-      page,
-    },
-  };
 }
